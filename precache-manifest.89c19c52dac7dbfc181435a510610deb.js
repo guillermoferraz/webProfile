1d72f491d92616e9edbf910f4e7eeb6187d38fcc
h19 <guillermoferrazdst@gmail.com>
2020-09-25 02:12:58 -0300

    Updates

---
+++ precache-manifest.89c19c52dac7dbfc181435a510610deb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a396ab69e469a990acf1bc12b248f55",
+    "url": "/webProfile/index.html"
+  },
+  {
+    "revision": "d85a697f9e3969a8d1c0",
+    "url": "/webProfile/static/css/main.cd616f40.chunk.css"
+  },
+  {
+    "revision": "e40c2b660b89031df078",
+    "url": "/webProfile/static/js/2.8298230e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/webProfile/static/js/2.8298230e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d85a697f9e3969a8d1c0",
+    "url": "/webProfile/static/js/main.fc3e4811.chunk.js"
+  },
+  {
+    "revision": "ed19a315b6bf6030a741",
+    "url": "/webProfile/static/js/runtime-main.8552924c.js"
+  },
+  {
+    "revision": "81d65679dacf1d3bffc57a1739cdfee3",
+    "url": "/webProfile/static/media/geometry.81d65679.jpg"
+  },
+  {
+    "revision": "7b06703a51cee9575ef66d6de8d02c99",
+    "url": "/webProfile/static/media/myProfileGF.7b06703a.jpg"
+  }
+]);
